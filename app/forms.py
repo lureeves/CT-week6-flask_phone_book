880cd429bbdb841a4bb339daaeadd66466607711
icncode <ljr77777@gmail.com>
2023-03-24 14:37:34 -0700

    Added signup page and updated number entry page

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms import StringField, TextAreaField, SubmitField, EmailField, PasswordField
+from wtforms.validators import InputRequired, EqualTo
 
 
-class SignUpForm(FlaskForm):
+class NumberEntry(FlaskForm):
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
     number = StringField('Number',  validators=[InputRequired()])
     address = TextAreaField('Address', validators=[InputRequired()])
-    submit = SubmitField('Submit Information')
+    submit = SubmitField('Submit Address')
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
-from app.forms import SignUpForm
+from app.forms import NumberEntry, SignUpForm
 from app.models import Address
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/information_entry', methods =["GET", "POST"])
-def information_entry():
+@app.route('/number_entry', methods =["GET", "POST"])
+def number_entry():
     # Create an instance of the form (in the context of the current request)
-    form = SignUpForm()
+    form = NumberEntry()
     # Check if the form was submitted and that all of the fields are valid 
     if form.is_submitted():
         # If so, get the data from the form fields
@@ -24,6 +24,16 @@ def information_entry():
         new_contact = Address(first_name=first_name, last_name=last_name, number=number, address=address)
         flash(f"{new_contact.first_name} {new_contact.last_name} has been added to the phonebook!", "success")
         return redirect(url_for('index'))
-    return render_template('information_entry.html', form=form)
+    return render_template('number_entry.html', form=form)
 
 
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
+
+
+
+@app.route('/login')
+def login():
+    return print('login clicked')
